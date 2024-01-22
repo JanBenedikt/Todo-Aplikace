1b2b038a19b8753edd8bb9120b944b633128565a
Borec191 <115628041+JanBenedikt@users.noreply.github.com>
2024-01-22 13:45:00 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+<div className='text'>
+ <h1>Seznam Úkolů</h1>
+  <div class='buttons  d-grid gap-3 col-2 mx-auto'>
+ <button type="button" class="btn btn-outline-primary">Přidat úkol</button>
+<button type="button" class="btn btn-outline-secondary">Odstranit Úkol</button>
 
+</div>
+
+<div class="container">
+ <div class="card">
+  <div class="card-body">
+   <h3 class="card-title">Úkol</h3>
+    <div class='d-grid gap-3 mx-auto'>
+     <button type="button" class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#úkol">Zobrazit seznam úkolů </button>
+     <a href="#" class="btn btn-danger mt-2">Odstranit úkol</a>
+    </div>
+   </div>
+  </div>
+ </div>   
+</div>
+)}
 export default App;
